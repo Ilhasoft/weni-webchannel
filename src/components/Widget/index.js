99ad601dbe534db99a0b235c10f4fe567534f7d9
Matheus Cristian <mathues@email.com>
2025-10-09 15:51:17 -0300

    fix(carousel): removes unnecessary code since it was moved to message

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { xml2js } from 'xml-js';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Sound from 'react-sound';
@@ -420,47 +419,9 @@ class Widget extends Component {
         ...receivedMessage.message,
         quick_replies: buildQuickReplies(receivedMessage.message.quick_replies)
       };
-      const xmlNotation = '<?xml version="1.0" encoding="UTF-8" ?>';
 
-      if (String(newMessage.text).includes(xmlNotation)) {
-        const text = String(newMessage.text).split(xmlNotation)[0];
-        const xml = xml2js(`<root>${String(newMessage.text).split(xmlNotation)[1]}</root>`);
-
-        if (text.trim() !== '') {
-          this.handleMessageReceived({
-            ...newMessage,
-            text
-          });
-        }
-
-        const elements = xml.elements.find(element => element.name === 'root').elements.filter(element => element.name === 'carousel-item').map(element => element.elements).map(elements => elements.reduce((acc, curr) => ({ ...acc, [curr.name]: curr.elements.find(element => element.type === 'text').text }), {}));
-
-        this.handleMessageReceived({
-          ...newMessage,
-          text: `\`\`\`html
-<section class="push-markdown-carousel">
-  ${elements.map(element => `
-  <section class="push-markdown-carousel__item">
-    <a class="push-markdown-carousel__item__link" href="${element.product_link}" target="_blank">
-      <img class="push-markdown-carousel__item__image" src="${element.image.match(/\(.+\)/)[0].slice(1, -1)}" />
-    </a>
-    <a class="push-markdown-carousel__item__link" href="${element.product_link}" target="_blank">
-      <h3 class="push-markdown-carousel__item__name" title="${element.name}">${element.name}</h3>
-    </a>
-    <a class="push-markdown-carousel__item__link" href="${element.product_link}" target="_blank">
-      <p class="push-markdown-carousel__item__description" title="${element.description}">${element.description}</p>
-    </a>
-    <p class="push-markdown-carousel__item__price">${element.price.replace(/(\(.+\))/, '<s>$1</s>')}</p>
-  </section>`).join('\n')}
-</section>
-`
-        });
-
-        this.emitSocketEvent('incomingMessage', newMessage);
-      } else {
-        this.handleMessageReceived(newMessage);
-        this.emitSocketEvent('incomingMessage', newMessage);
-      }
+      this.handleMessageReceived(newMessage);
+      this.emitSocketEvent('incomingMessage', newMessage);
     } else if (receivedMessage.type === 'typing_start') {
       if (this.typingTimeoutId) {
         clearTimeout(this.typingTimeoutId);
